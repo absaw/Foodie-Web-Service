acda6904fa04a4a130d14cb5b4490c3dab45c83a
Abhishek Sawalkar <abhishek31sawalkar@gmail.com>
2024-09-08 17:56:44 -0400

    React frontend and django backend connected

--- storefront/storefront/urls.py
+++ storefront/storefront/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic import TemplateView
 # import playground
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('playground.urls'))
+    path('',TemplateView.as_view(template_name='index.html')),
+    # path('',include('playground.urls'))
 ]
